e95c387442fddfb902fa2bda48702d299b9ae521
tatara15 <854355617@qq.com>
2019-03-04 21:03:48 +0800

    重新上传缺少依赖2019年3月4日21:03:46

---
+++ yeweb_admin/admin/src/components/Home.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="home">
+        <el-container>
+         <!-- 侧边栏    -->
+        <el-aside width="auto">
+            <div class="logo">
+                logo
+            </div>
+            chelan
+        </el-aside>
+        <el-container>
+            <!-- 头部 -->
+            <el-header>
+                toubu
+            </el-header>
+            <!-- 主体 -->
+            <el-main>
+                <router-view></router-view>
+            </el-main>
+        </el-container>
+        </el-container>
+    </div>
+</template>
+
+<script>
+export default {
+    data() { 
+        return {
+        }
+    },
+    methods:{
+       
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.home {
+    height: 100%;
+    .el-menu-vertical-demo:not(.el-menu--collapse) {
+        width: 200px;
+        min-height: 400px;
+    }
+    .el-container {
+        height: 100%;
+    }
+    .el-aside {
+        background-color: #c4e0f6;
+    }
+    .el-header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        background-color: #c4e0f9;
+    }
+    .logo {
+        height:60px;
+        // background: url(../images/logo.jpg);
+        background-size: cover;
+        background-color: #989898;
+    }
+    .toggle-btn {
+        font-size: 36px;
+        color: #989898;
+        cursor: pointer;
+        line-height: 60px;
+    }
+    .system-title {
+        font-size: 28px;
+        color: #1e1e1e;
+    }
+    .welcome, {
+        color: #1e1e1e;
+    }
+}
+</style>
--- yeweb_admin/admin/src/main.js
+++ yeweb_admin/admin/src/main.js
@@ -3,7 +3,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+//导入vuex
+import vuex from 'vuex'
+//导入EU相关文件
+import  'element-ui/lib/theme-chalk/index.css'
+import element from 'element-ui/lib'
+Vue.config.productionTip = false
+//挂载
+Vue.use(element)
+Vue.use(vuex)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
